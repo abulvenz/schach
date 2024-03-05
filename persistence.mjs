93bb484075fe1412dc14bed629612b01193a51bd
Andreas Eismann <a.eismann@senbax.de>
2024-03-05 10:41:30 +0100

    Added archive for finished games.

--- persistence.mjs
+++ persistence.mjs
@@ -38,6 +38,33 @@ const persistence = {
       await client.hSet("games", game.current().id, str);
     },
   },
+  archive: {
+    loadById: async (id) => {
+      const game = await client.hGet("archive", id);
+      console.log("loaded", game);
+      if (game === undefined) {
+        return undefined;
+      }
+      const st = stack();
+      try {
+        st.fromStr(game);
+      } catch (e) {
+        console.error(e);
+        return undefined;
+      }
+      return st;
+    },
+    loadAllKeys: async () => {
+      const games = await client.hKeys("archive");
+      console.log("loaded", games);
+      return games;
+    },
+    save: async (game) => {
+      const str = game.toStr();
+      console.log("saving", str);
+      await client.hSet("archive", game.current().id, str);
+    },
+  },
 };
 
 export default persistence;
