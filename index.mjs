29263cd2b905b11ba3236605c288a59dc2e92bc3
Andreas Eismann <a.eismann@senbax.de>
2024-03-05 10:19:38 +0100

    Added persistence in redis.

--- index.mjs
+++ index.mjs
@@ -8,6 +8,7 @@ import * as jsonpatch from "fast-json-patch/index.mjs";
 import field from "./field.mjs";
 import stack from "./stack.mjs";
 import GameProxy from "./gameproxy.mjs";
+import persistence from "./persistence.mjs";
 
 const app = express();
 const server = createServer(app);
@@ -40,70 +41,101 @@ const shuffle = (arr, r = []) =>
   );
 
 const users = [];
-const games = {};
-let next = 0;
 
 io.on("connection", (socket) => {
-  const useridx = ++next;
-  users.push({ socket, idx: useridx }) - 1;
-  io.emit("hi", { id: useridx, users: users.map((u) => u.idx) });
-  console.log(
-    "a user connected",
-    users.map((u) => u.idx)
-  );
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-    users.splice(
-      users.findIndex((e) => e.idx === useridx),
-      1
+  socket.on("i am", async (msg) => {
+    console.log("i am", msg);
+    const useridx = msg.id;
+    if (users.findIndex((u) => u.idx === useridx) < 0)
+      users.push({ socket, idx: useridx });
+    io.emit("hi", { id: useridx, users: users.map((u) => u.idx) });
+    console.log(
+      "a user connected",
+      users.map((u) => u.idx)
     );
+
+    io.emit("games", await persistence.games.loadAllKeys());
+
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
+      users.splice(
+        users.findIndex((e) => e.idx === useridx),
+        1
+      );
+      io.emit("goodbye", { id: useridx, users: users.map((u) => u.idx) });
+    });
+    socket.on("chat message", (msg) => {
+      io.emit("chat message", msg);
+    });
+
+    socket.on("game", async (msg) => {
+      const f = field.initialField.slice();
+      const gameid = `game-${useridx}-${msg.user}`;
+      const game = stack({
+        id: gameid,
+        playerB: useridx,
+        playerW: msg.user,
+        field: f,
+        valid: [],
+        next: "W",
+        result: undefined,
+        selected: undefined,
+      });
+      console.log("creating game", gameid);
+      socket.join(gameid);
+      users.find((u) => u.idx === msg.user).socket.join(gameid);
+
+      io.emit("games", await persistence.games.loadAllKeys());
+      persistence.games.save(game);
+      io.to(gameid).emit("field", game.pop());
+    });
+
+    socket.on("select", async (msg) => {
+      const game = await persistence.games.loadById(msg.gameid);
+      if (game === undefined) {
+        return;
+      }
+      console.log(game);
+      const g = game.current();
+
+      console.log(socket.rooms);
+
+      const proxy = GameProxy(g);
+
+      if (!proxy.validPlayer(useridx)) {
+        return;
+      }
+
+      proxy.handleSelection(msg.selected);
+
+      game.push(proxy.newState());
+      io.to(g.id).emit("field", g);
+      persistence.games.save(game);
+    });
+
+    socket.on("undo", async (msg) => {
+      const game = await persistence.games.loadById(msg.gameid);
+      if (game === undefined) {
+        return;
+      }
+      const g = game.pop();
+      if (g.playerB !== useridx && g.playerW !== useridx) {
+        return;
+      }
+      persistence.games.save(game);
+      io.to(g.id).emit("field", g);
+    });
+
+    socket.on("enter game", async (msg) => {
+      const game = await persistence.games.loadById(msg.gameid);
+      if (game === undefined) {
+        return;
+      }
+      socket.join(msg.gameid);
+      const g = game.current();
+      io.to(msg.gameid).emit("field", g);
+    });
   });
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
-  });
-
-  socket.on("game", (msg) => {
-    const f = field.initialField.slice();
-    const gameid = `game-${useridx}-${msg.user}`;
-    const game = (games[gameid] = stack({
-      id: gameid,
-      playerB: useridx,
-      playerW: msg.user,
-      field: f,
-      valid: [],
-      next: "W",
-      selected: undefined,
-    }));
-
-    socket.join(game.id);
-    users.find((u) => u.idx === msg.user).socket.join(game.id);
-
-    io.to(game.id).emit("field", game.pop());
-  });
-
-  socket.on("select", (msg) => {
-    const game = games[msg.gameid];
-    const g = game.current();
-
-    const proxy = GameProxy(g);
-
-    if (!proxy.validPlayer(useridx)) {
-      return;
-    }
-
-    proxy.handleSelection(msg.selected);
-
-    game.push(proxy.newState());
-    io.to(game.id).emit("field", g);
-  });
-
-  socket.on("undo", (msg) => {
-    const game = games[msg.gameid];
-    const g = game.pop();
-    io.to(game.id).emit("field", g);
-  });
-
-  socket.on("enter", (msg) => socket.join(msg));
 });
 
 server.listen(4000, () => {
---
+++ persistence.mjs
@@ -0,0 +1,43 @@
+import redis from "redis";
+
+import { createClient } from "redis";
+import stack from "./stack.mjs";
+
+const client = await createClient()
+  .on("ready", () => console.info("Redis Client Connected"))
+  .on("error", (err) => console.log("Redis Client Error", err))
+  .connect();
+
+client.HGETALL("games").then((e) => console.log(e));
+
+const persistence = {
+  games: {
+    loadById: async (id) => {
+      const game = await client.hGet("games", id);
+      console.log("loaded", game);
+      if (game === undefined) {
+        return undefined;
+      }
+      const st = stack();
+      try {
+        st.fromStr(game);
+      } catch (e) {
+        console.error(e);
+        return undefined;
+      }
+      return st;
+    },
+    loadAllKeys: async () => {
+      const games = await client.hKeys("games");
+      console.log("loaded", games);
+      return games;
+    },
+    save: async (game) => {
+      const str = game.toStr();
+      console.log("saving", str);
+      await client.hSet("games", game.current().id, str);
+    },
+  },
+};
+
+export default persistence;
